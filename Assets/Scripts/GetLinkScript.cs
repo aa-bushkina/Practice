fe31732da72807f8658d2d5b2440c259b023a0bd
Alena Bushkina <aa-bushkina@mail.ru>
2022-07-09 03:18:13 +0300

    Do downloading of csv file on desktop

--- Assets/Scripts/GetLinkScript.cs
+++ Assets/Scripts/GetLinkScript.cs
@@ -1,5 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -26,10 +31,27 @@ public class GetLinkScript : MonoBehaviour
 
     public void OnClick()
     {
-        bool isLinkOk = ParseInput(_inputField.text); 
+        bool isLinkOk = ParseInput(_inputField.text);
         if (!isLinkOk)
         {
             PrintLinkError();
+        }
+
+        var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        var fullPath = Path.Combine(folderPath, "values.csv");
+        string uri = string.Concat(_linkText.Substring(0, _linkText.LastIndexOf("/edit")), "/export?exportFormat=csv");
+
+        //"https://docs.google.com/spreadsheets/d/1VXKvao9SVMp6O4REm0i8dvQNwKti5CshEj2yW2ExWzE/export?exportFormat=csv";
+        //https://docs.google.com/spreadsheets/d/1VXKvao9SVMp6O4REm0i8dvQNwKti5CshEj2yW2ExWzE/edit#gid=0
+        using (var client = new HttpClient())
+        {
+            using (var s = client.GetStreamAsync(uri))
+            {
+                using (var fs = new FileStream(fullPath, FileMode.OpenOrCreate))
+                {
+                    s.Result.CopyTo(fs);
+                }
+            }
         }
 
         PrintOkMessage();
@@ -37,11 +59,9 @@ public class GetLinkScript : MonoBehaviour
 
     private bool ParseInput(in string inputText)
     {
-
         //проверка норм ли ссылка
         if (1 == 0 /*не норм*/)
         {
-            
             return false;
         }
 
@@ -54,10 +74,10 @@ public class GetLinkScript : MonoBehaviour
         //выводим окошко с ошибкой
         _resultText.text = "Not Ok";
     }
-    
+
     public void PrintOkMessage()
     {
         //выводим окошко, что все ок
-        _resultText.text = _linkText;
+        _resultText.text = "Ok";
     }
 }
